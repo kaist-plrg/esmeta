579aa21f340d3dde56b021cb6059b2ce552fc951
Jihee Park <j31d0@users.noreply.github.com>
2022-04-18 23:42:11 +0900

    Draw call graph

--- src/main/scala/esmeta/editor/callgraph/CallGraph.scala
+++ src/main/scala/esmeta/editor/callgraph/CallGraph.scala
@@ -8,4 +8,14 @@ trait CallGraph:
   // def calls: Map[String, Set[Call]]
   // def call_targets: Map[Call, Set[String]]
   def func_targets: Map[String, Set[String]]
+
+  def toDot: String =
+    val mlist = (funcs ++ func_targets.values.flatten).toList.zipWithIndex.toMap
+    s"""
+digraph {
+${mlist.keySet.map((s) => s"\"X${mlist(s)}\" [label=\"$s\"]").mkString("\n")}
+
+${funcs.map((s) => func_targets.getOrElse(s, Set()).map((t) => s"X${mlist(s)} -> X${mlist(t)}").mkString("\n")).mkString("\n")}
+}
+"""
 end CallGraph
--- src/main/scala/esmeta/phase/ViewPEval.scala
+++ src/main/scala/esmeta/phase/ViewPEval.scala
@@ -8,6 +8,7 @@ import esmeta.editor.sview.SyntacticView
 import esmeta.cfg.CFG
 import esmeta.editor.PartialEval
 import esmeta.editor.util.CFGHelper
+import java.io.PrintWriter
 
 /** `view-peval` phase */
 case object ViewPEval extends Phase[(CFG, SyntacticView), Unit] {
@@ -20,7 +21,9 @@ case object ViewPEval extends Phase[(CFG, SyntacticView), Unit] {
   ): Unit =
     val (cfg, view) = cfgWithview
     val peval = PartialEval(CFGHelper(cfg), verbose = true)
-    peval(view)
+    val writer = PrintWriter("peval.dot")
+    writer.println(peval.cg(view).toDot)
+    writer.close
   def defaultConfig: Config = Config()
   val options: List[PhaseOption[Config]] = List()
   case class Config()

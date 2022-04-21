982637314605fd849c842c998f54bb73231071b8
h2oche <h2oche22@gmail.com>
2022-04-21 13:00:33 +0900

    Fix filter for soundness check

--- src/main/scala/esmeta/editor/util/Filter.scala
+++ src/main/scala/esmeta/editor/util/Filter.scala
@@ -13,43 +13,29 @@ object Filter {
   import JsonProtocol.given
   import JsJsonProtocol.given
 
-  def apply(
+  def getAlgos(
     cfg: CFG,
     dataDir: String,
     sview: SyntacticView,
-  ): (List[String], Set[Int]) = {
+  ): Set[Int] = {
     val tests = readFile(s"$dataDir/test262-list").split(LINE_SEP)
-    var testSet: Set[String] = Set()
     var algoSet: Set[Int] = Set()
+
     val progress = ProgressBar("filter test262", 0 until tests.size)
-
-    var t0 = 0L
-    var t1 = 0L
-
     for (idx <- progress) {
-      val (ltime, (annoMap, algoMap, astList)) =
-        time(
-          readJson[(Map[Int, Set[Annotation]], Map[Int, Set[Int]], List[Ast])](
-            s"$dataDir/data/$idx.json",
-          ),
+      val (annoMap, algoMap, astList) =
+        readJson[(Map[Int, Set[Annotation]], Map[Int, Set[Int]], List[Ast])](
+          s"$dataDir/data/$idx.json",
         )
 
-      val (ctime, _) = time(
-        for {
-          ast <- astList
-          conc <- ast.getConcreteParts(sview, annoMap)
-          astId <- conc.idOpt
-          aids <- algoMap.get(astId)
-        } { algoSet ++= aids; testSet += tests(idx) },
-      )
-
-      t0 += ltime
-      t1 += ctime
+      for {
+        ast <- astList
+        conc <- ast.getConcreteParts(sview, annoMap)
+        astId <- conc.idOpt
+        aids <- algoMap.get(astId)
+      } algoSet ++= aids
     }
 
-    println(t0)
-    println(t1)
-
-    (testSet.toList.sorted, algoSet)
+    algoSet
   }
 }

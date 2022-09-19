fb96677131fa51ed5e4d4098241be1ecf36d0c4d
DJ <f52985@gmail.com>
2022-09-19 19:52:27 +0900

    Implement ast validity checker

--- src/main/scala/esmeta/es/util/AstValidityChecker.scala
+++ src/main/scala/esmeta/es/util/AstValidityChecker.scala
@@ -2,8 +2,13 @@ package esmeta.es.util
 
 import esmeta.es.*
 import esmeta.spec.*
+import esmeta.util.JSEngine
 
 /** TODO AST validity checker */
 object AstValidityChecker {
-  def apply(grammar: Grammar, ast: Ast): Boolean = true
+  def apply(grammar: Grammar, ast: Ast): Boolean =
+    val MESSAGE = "VALIDITY_CHECKER_EXPECTED_EXCEPTION"
+    val src = s"\"use strict\";\nthrow $MESSAGE;\n${ast.toString(grammar)}"
+
+    JSEngine.run(src).failed.filter(_.getMessage contains MESSAGE).isSuccess
 }

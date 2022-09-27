42309fad068accbb7fed722ab83b51f6b39fbc1f
jhnaldo <jhpjhp0223@gmail.com>
2022-09-27 11:58:02 +0900

    Added -parse:pprint option for pretty print

--- src/main/scala/esmeta/phase/Parse.scala
+++ src/main/scala/esmeta/phase/Parse.scala
@@ -17,7 +17,9 @@ case object Parse extends Phase[Spec, Ast] {
     config: Config,
   ): Ast =
     val filename = getFirstFilename(cmdConfig, name)
-    ESParser(spec.grammar, config.debug)("Script").fromFile(filename)
+    val ast = ESParser(spec.grammar, config.debug)("Script").fromFile(filename)
+    if (config.pprint) println(ast.toString(spec.grammar))
+    ast
   def defaultConfig: Config = Config()
   val options: List[PhaseOption[Config]] = List(
     (
@@ -25,8 +27,14 @@ case object Parse extends Phase[Spec, Ast] {
       BoolOption(c => c.debug = true),
       "turn on debugging mode.",
     ),
+    (
+      "pprint",
+      BoolOption(c => c.pprint = true),
+      "print pretty string of parsed source code.",
+    ),
   )
   case class Config(
     var debug: Boolean = false,
+    var pprint: Boolean = false,
   )
 }

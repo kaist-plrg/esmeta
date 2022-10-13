0fc8fdd30b4d9e93879bf6f380f37b0c9a903bd8
DJ <f52985@gmail.com>
2022-10-14 01:08:50 +0900

    Implement localization

--- src/main/scala/esmeta/phase/Localize.scala
+++ src/main/scala/esmeta/phase/Localize.scala
@@ -32,10 +32,40 @@ case object Localize extends Phase[Unit, Any] {
       nodeViewCoverageRaw.map { case (v, Info(f, _)) => v -> f }
     val id2NodeView: Map[Int, NodeView] =
       nodeViewCoverageRaw.map { case (v, Info(_, i)) => i -> v }
-    val touchedNodeView: Map[Name, Vector[Int]] =
+    val touchedNodeViewMap: Map[Name, Vector[Int]] =
       readJson(touchedNodeViewJson)
-    val failsMap: Map[Target, Vector[Name]] =
+    val failsMap: Map[Target, Set[Name]] =
       readJson(failsMapJson)
+
+    val total = touchedNodeViewMap.keys.toVector
+    val nodes = nodeViewCoverageRaw.keys.toVector
+
+    failsMap.foreach((target, fails) => {
+      val passNum = total.size - fails.size
+      val failNum = fails.size
+
+      // initial stat map
+      type StatMap = Map[NodeView, NodeStat]
+      val initStatMap = nodes.map(_ -> NodeStat(0, 0, passNum, failNum)).toMap
+
+      // update stat of each node by iterating all tests
+      val statMap = total.foldLeft(initStatMap) {
+        case (statMap, test) =>
+          val isPass = !fails.contains(test)
+          val touchedNodeViews = touchedNodeViewMap(test).map(id2NodeView)
+
+          def updateStatMap(statMap: StatMap, nodeView: NodeView) =
+            statMap.updatedWith(nodeView)(_.map(_.touch(isPass)))
+
+          touchedNodeViews.foldLeft(statMap)(updateStatMap)
+      }
+
+      val sorted = statMap.toSeq.sortBy(_._2.score).reverse
+      sorted
+        .slice(0, 10)
+        .foreach((k, v) => println(s"${nodeView2Function(k)}: ${v.score}"))
+      // TODO: function aggregation
+    })
 
   /** used to parse node-coverage json */
   case class Info(func: Func, id: Int = id)
@@ -47,6 +77,17 @@ case object Localize extends Phase[Unit, Any] {
     final def apply(c: HCursor): Decoder.Result[Info] =
       for (func <- c.downField("func").as[String])
         yield Info(func)
+  }
+
+  /** quadruple of node stat */
+  case class NodeStat(ep: Int, ef: Int, np: Int, nf: Int) {
+    def touch(isPass: Boolean): NodeStat = {
+      if (isPass)
+        NodeStat(ep + 1, ef, np - 1, nf)
+      else
+        NodeStat(ep, ef + 1, np, nf - 1)
+    }
+    lazy val score = ef - ep / (ep + np + 1.0)
   }
 
   def defaultConfig: Config = Config()

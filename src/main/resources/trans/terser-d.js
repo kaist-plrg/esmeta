97f644e8b176ccdff5456ada3217c391cc36f18a
DJ <f52985@gmail.com>
2022-10-29 00:14:09 +0900

    Enable compress mode for terser

--- src/main/resources/trans/terser-d.js
+++ src/main/resources/trans/terser-d.js
@@ -6,7 +6,6 @@ const { transpileAsyncUsing } = require("./trans-d.js");
 /** Do transpile using terser */
 let transpileAsync = async (input) => {
   return (await terser.minify(input, {
-    compress: false,
     mangle: false,
   })).code;
 }

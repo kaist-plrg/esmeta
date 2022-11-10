dc42926dcb736cf22da00bcfde4a4766e6348e29
DJ <f52985@gmail.com>
2022-11-11 01:59:21 +0900

    Fix bug in clean-TODO

--- src/main/resources/bugs/clean-todo.js
+++ src/main/resources/bugs/clean-todo.js
@@ -7,13 +7,13 @@ for(dir of dirs) if(fs.lstatSync(dir).isDirectory()) {
 
   let knowns = fs.readdirSync(dir)
     .filter(name => name.endsWith(".js"))
-    .map(js => fs.readFileSync(path.join(dir, js), "utf8").trim().split("\n"))
+    .map(js => fs.readFileSync(path.join(dir, js), "utf8").trim().split("\n").map(l => l.trim()))
 
   let todoDir = path.join(dir, "TODO");
   try {
     const todos = fs.readdirSync(todoDir);
     for(todo of todos) {
-      let target = fs.readFileSync(path.join(todoDir, todo), "utf8").trim().split("\n")[1];
+      let target = fs.readFileSync(path.join(todoDir, todo), "utf8").trim().split("\n")[1].trim();
       for(lines of knowns) {
         if(lines.includes(target)) {
           console.log(todoDir, todo, target);

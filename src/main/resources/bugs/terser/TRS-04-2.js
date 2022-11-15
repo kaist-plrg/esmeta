5cbdb2259cf8d4fec9327203eef0ee8a7f462632
DJ <f52985@gmail.com>
2022-11-15 22:02:28 +0900

    Update bug DB

--- src/main/resources/bugs/terser/TRS-04-2.js
+++ src/main/resources/bugs/terser/TRS-04-2.js
@@ -1,6 +1,26 @@
+0 ^ 1n >>> 1n ; 
 0n >>> 0n ;
+0n >>> 0n > 0 ; 
+0n >>> 1n & 0 ; 
 0n >>> 1n ;
+0n >>> 1n >>> 0 ; 
+0n >>> 1n ?? 0 ; 
+1n >>> 0 ;
 1n >>> 0n ;
 1n >>> 1n ;
+1n >>> 1n < 0 ; 
+1n >>> 1n << 0 ; 
+1n >>> 1n === 0 ; 
 ; [ 0n ] [ 0 ] >>>= 1n ;
-1n >>> 0 ;
+[ ... 1n >>> 0n ] ; 
+[ 0 , , 1n >>> 0n ] ; 
+[ 0 ^ ~ 0n >>> 0n ] ; 
+[ 1n >>> 1n === 0 ] ; 
+[ 1n >>> 1n ] ; 
+`${ 0 }${ 0 instanceof 1n >>> 0n }` ; 
+for ( 0n >>> 0n ; 0 ; ) ; 
+new function ( ) { ; return [ 0n ] [ 0 ] >>>= 0n ; } ; 
+switch ( 0 ) { case 0n >>> 0n : default : case 0 : } 
+switch ( 0 ) { case 1n >>> 1n : default : } 
+switch ( 0 ) { default : case 1n >>> 0n : } 
+switch ( 1n >>> 0n ) { } 
--- src/main/resources/bugs/terser/TRS-15.js
+++ src/main/resources/bugs/terser/TRS-15.js
@@ -1,114 +1,115 @@
+'' [ 0 ] ??= await ( ) ;
+( await ) ; async function * await ( ) { }
 0 % await ( ) ; 
+0 ?. ( 0 , await ) ; function * await ( ) { }
+0 || 0 > await ( ) ;
+[ , 0 !== await ] ; function await ( ) { }
+[ , 0 , , ... { [ Symbol . iterator ] : function * ( x ) { yield * await + 0 ; } } ] ;
+[ , 0 , , ... { [ Symbol . iterator ] : function * ( x ) { yield await ( ) ; ; } } ] ;
+[ , 0 , , ... { [ Symbol . iterator ] : function * ( x ) { yield new await ( ) ; } } ] ;
+[ ... await + 0 ] ;
+[ 0 , , 0 . x %= await ( ) ] ;
+[ 0 , , await ] ; async function await ( ) { }
+[ 0 , , function * ( ) { } ( ) ( 0 , await ) ] ; function * await ( ) { }
+`${ 0 }${ 0 != await instanceof 0 }${ 0 }` ; function await ( ) { }
 `${ 0 }${ 0 }${ await ( ) }` ; 
+async function * await ( ) { } new class { 0 = super [ await && 0 ] ; } ;
+async function * await ( ) { } switch ( await ) { case x = 0 : default : }
+async function * await ( ) { } var { 0 : [ ] = await . x } = 0 ;
+async function * await ( ) { } while ( await == { } ) ;
+async function await ( ) { } 0 > 0 in await ;
+async function await ( ) { } `${ 0 }${ 0 }${ 0 in await }${ 0 }` ;
+async function await ( ) { } for ( var x of ~ ! await ) ;
+async function await ( ) { } new class { 0 = await ; } ;
+async function await ( ) { } throw await &&= 0 ;
+await &&= 0 ; function await ( ) { }
 await ( ) ; 
+await ( ) == 0 ;
 await ( ) >= 0 ; 
+await + 0 - 0 ;
+await + 0 instanceof 0 ;
+await ? 0 : 0 ; function await ( ) { }
+await ?. x ; async function await ( ) { }
+class x extends await ( ) { ; }
+class x extends await ( ) { }
+class x extends await { } function await ( ) { }
+class x { [ await ( ) ] ; }
+class x { [ await ( ) ] = 0 ; }
+for ( 0 instanceof new await ( ) ; ; ) ; function await ( ) { return ; }
+for ( ; await ( ) ; 0 ) ;
+for ( ; await = 0 . x ; 0 ) ; async function await ( ) { }
+for ( await instanceof new await ( ) ; ; ) ; function await ( ) { }
+for ( let [ [ ] = await , x , , ] of [ `` ] ) ; async function await ( ) { }
+for ( let x ; ; 0 ) var [ ] = await ( ) ;
 for ( let x ; await ( ) ; ) ; 
 for ( let x ; await ( ) ; 0 ) ; 
 for ( let x ; await + 0 ; ) ; 
 for ( let x ; await + 0 ; 0 ) ; 
 for ( let x in await ( ) ) ; 
+for ( let x in await ) ; async function await ( ) { }
+for ( let x in await + 0 ) ;
+for ( let x of [ 0 ] ) try { throw 0 ; } catch { await ( 0 ) ; } 
 for ( let x of await ( ) ) ; 
 for ( let x of await + 0 ) ; 
+for ( var x ; ++ await + 0 ; 0 ) ;
 for ( var x ; ; await ( ) ) ; 
 for ( var x ; await ( ) ; ) ; 
 for ( var x ; await ( ) ; 0 ) ; 
+for ( var x ; await + 0 ; ) ;
 for ( var x in await ( ) ) ; 
+for ( var x in await + 0 ) ;
+for ( var x in { x } ) if ( await ( ) ) ;
 for ( var x of await ( ) ) ; 
 for ( var x of await + 0 ) ; 
-{ } [ void await + '' ] ; 
-[ ... await + 0 ] ;
-var x = await + 0 ;
-let x , await = new '' [ await + 0 % 0 ] ;
-await + 0 instanceof 0 ;
-new class x { 0 = await + 0 ; } ;
-for ( var x ; await + 0 ; ) ;
-for ( var x ; ++ await + 0 ; 0 ) ;
-var { ... x } = await + 0 ;
-for ( let x in await + 0 ) ;
-for ( var x in await + 0 ) ;
-[ , 0 , , ... { [ Symbol . iterator ] : function * ( x ) { yield * await + 0 ; } } ] ;
-var { x } = await + 0 ;
-{ if ( ! + await + 0 ) ; }
-var { x = await + 0 } = 0 ;
-var [ x = await + 0 ] = `` ;
-let x = await + 0 ;
-await + 0 - 0 ;
-function await ( ) { } x : switch ( await ) { }
-{ [ ] = await ( ) ; }
-function await ( ) { } await ;
-var [ x = await && 0 ] = `` ; function await ( ) { }
-function await ( ) { } 0 ( 0 , ... await ) ;
-let x , await = await ( ) ;
-function await ( ) { } for ( await [ 0 ] in 0 , [ 0 , , ] ) ;
-for ( 0 instanceof new await ( ) ; ; ) ; function await ( ) { return ; }
-class x { [ await ( ) ] = 0 ; }
-for ( let x ; ; 0 ) var [ ] = await ( ) ;
-await &&= 0 ; function await ( ) { }
-{ x = await ?? 0 ; async function * await ( ) { } } ;
-for ( ; await ( ) ; 0 ) ;
-function await ( ) { } for ( await [ 0 ] in 0 , await ) ;
-for ( ; await = 0 . x ; 0 ) ; async function await ( ) { }
-async function * await ( ) { } var { 0 : [ ] = await . x } = 0 ;
-x : if ( '' . x ||= await ) ; function await ( ) { }
-switch ( 0 ) { case await ( ) : }
-'' [ 0 ] ??= await ( ) ;
-switch ( 0 ) { case null ?? await : default : case 0 : async function * await ( ) { } }
-let x = 0 < import ( await ( ) ) ?. x ;
-0 || 0 > await ( ) ;
-function await ( ) { } [ 0 !== 0 !== await , 0 ] ;
-[ , 0 , , ... { [ Symbol . iterator ] : function * ( x ) { yield await ( ) ; ; } } ] ;
-async function * await ( ) { } while ( await == { } ) ;
-function await ( ) { } for ( let x of 0 in await ) ;
-var [ x = await ( ) ] = `` ;
-var { x = await ( ) } = 0 ;
-async function await ( ) { } for ( var x of ~ ! await ) ;
-class x { [ await ( ) ] ; }
-function * await ( ) { } await ??= 0 ;
-async function await ( ) { } 0 > 0 in await ;
-let x = await ( ) ;
-async function await ( ) { } throw await &&= 0 ;
-for ( let [ [ ] = await , x , , ] of [ `` ] ) ; async function await ( ) { }
-async function * await ( ) { } new class { 0 = super [ await && 0 ] ; } ;
-let [ x = await ( ) ] = '' ;
-async function await ( ) { } `${ 0 }${ 0 }${ 0 in await }${ 0 }` ;
-var x = await ( ) === 0 ;
-async function * await ( ) { } switch ( await ) { case x = 0 : default : }
-( await ) ; async function * await ( ) { }
-[ 0 , , await ] ; async function await ( ) { }
-var { x } = 0 . x ||= await ( ) ;
-function * await ( ) { } for ( let x of this ? await : 0 ) ;
-class x extends await ( ) { ; }
-var x = await ( ) ;
-function * await ( ) { } for ( let x of 0 ?. x ?. x ? await : 0 ) ;
-while ( await ( ... 0 ) ** 0 ) ; function * await ( ) { }
-[ 0 , , 0 . x %= await ( ) ] ;
-if ( await ) ; function * await ( ) { }
-class x extends await ( ) { }
-function * await ( ) { } for ( ; await ; 0 ) for ( var x of 0 ) ;
-for ( let x in await ) ; async function await ( ) { }
-var x = 0 [ 0 . x %= await = 0 ] %= 0 ; function await ( ) { }
-[ , 0 , , ... { [ Symbol . iterator ] : function * ( x ) { yield new await ( ) ; } } ] ;
-await ?. x ; async function await ( ) { }
-let { x = await ( ) } = 0 ;
-if ( await ) ; else ; async function await ( ) { }
-function * await ( ) { } let x = 0 <= ! 0 && await ;
-var { x } = await = 0 ; function await ( ) { }
 function * await ( ) { } 0 ( ... await ? 0 : 0 ) ;
-var x = { x = await , } = 0 ; async function * await ( ) { }
-function await ( ) { } ! { ... await } ;
-[ 0 , , function * ( ) { } ( ) ( 0 , await ) ] ; function * await ( ) { }
-0 ?. ( 0 , await ) ; function * await ( ) { }
 function * await ( ) { } [ 0 , , await . x = 0 ] ;
-async function await ( ) { } new class { 0 = await ; } ;
-class x extends await { } function await ( ) { }
-var { x } = await ( ) ;
-for ( var x in { x } ) if ( await ( ) ) ;
+function * await ( ) { } await ??= 0 ;
 function * await ( ) { } await ||= 0 ;
-for ( await instanceof new await ( ) ; ; ) ; function await ( ) { }
-await ? 0 : 0 ; function await ( ) { }
-{ function await ( ) { } x : var { x = await } = 0 ; }
-await ( ) == 0 ;
+function * await ( ) { } for ( ; await ; 0 ) for ( var x of 0 ) ;
+function * await ( ) { } for ( let x of 0 ?. x ?. x ? await : 0 ) ;
+function * await ( ) { } for ( let x of this ? await : 0 ) ;
+function * await ( ) { } let x = 0 <= ! 0 && await ;
+function await ( ) { } ! { ... await } ;
+function await ( ) { } 0 ( 0 , ... await ) ;
+function await ( ) { } [ 0 !== 0 !== await , 0 ] ;
+function await ( ) { } await ;
 function await ( ) { } await || '' ;
+function await ( ) { } for ( await [ 0 ] in 0 , [ 0 , , ] ) ;
+function await ( ) { } for ( await [ 0 ] in 0 , await ) ;
+function await ( ) { } for ( let x of 0 in await ) ;
 function await ( ) { } let [ ] = [ 0 , 0 , ] . x = [ 0 , , ] [ 0 ] ||= await ;
-[ , 0 !== await ] ; function await ( ) { }
-`${ 0 }${ 0 != await instanceof 0 }${ 0 }` ; function await ( ) { }
+function await ( ) { } x : switch ( await ) { }
+if ( await ) ; else ; async function await ( ) { }
+if ( await ) ; function * await ( ) { }
+let [ x = await ( ) ] = '' ;
+let x , await = await ( ) ;
+let x , await = new '' [ await + 0 % 0 ] ;
+let x = 0 < import ( await ( ) ) ?. x ;
+let x = await ( ) ;
+let x = await + 0 ;
+let { x = await ( ) } = 0 ;
+new class x { 0 = await + 0 ; } ;
+switch ( 0 ) { case await ( ) : }
+switch ( 0 ) { case null ?? await : default : case 0 : async function * await ( ) { } }
+var [ x = await && 0 ] = `` ; function await ( ) { }
+var [ x = await ( ) ] = `` ;
+var [ x = await + 0 ] = `` ;
+var x = 0 [ 0 . x %= await = 0 ] %= 0 ; function await ( ) { }
+var x = await ( ) ;
+var x = await ( ) === 0 ;
+var x = await + 0 ;
+var x = { x = await , } = 0 ; async function * await ( ) { }
+var { ... x } = await + 0 ;
+var { x = await ( ) } = 0 ;
+var { x = await + 0 } = 0 ;
+var { x } = 0 . x ||= await ( ) ;
+var { x } = await ( ) ;
+var { x } = await + 0 ;
+var { x } = await = 0 ; function await ( ) { }
+while ( await ( ... 0 ) ** 0 ) ; function * await ( ) { }
+x : if ( '' . x ||= await ) ; function await ( ) { }
+{ [ ] = await ( ) ; }
+{ function await ( ) { } x : var { x = await } = 0 ; }
+{ if ( ! + await + 0 ) ; }
+{ x = await ?? 0 ; async function * await ( ) { } } ;
+{ } [ void await + '' ] ; 

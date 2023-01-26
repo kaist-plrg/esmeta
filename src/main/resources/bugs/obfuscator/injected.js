e0fef00c1fee24b18e797d3dd7edc10989313ef9
KunJeong <kunjeong99@naver.com>
2023-01-26 15:44:21 +0900

    doConformTest for de-dedup

---
+++ src/main/resources/bugs/obfuscator/injected.js
@@ -0,0 +1,306 @@
+// [EXIT] normal
+! ( x => this . x ||= class x { } ) ( ) ;
+
+(() => {
+// hidden constructors
+let AsyncArrowFunction = Object.getPrototypeOf(async () => {}).constructor;
+let AsyncFunction = Object.getPrototypeOf(async function () {}).constructor;
+let AsyncGeneratorFunction = Object.getPrototypeOf(
+  async function* () {}
+).constructor;
+let GeneratorFunction = Object.getPrototypeOf(function* () {}).constructor;
+
+// logging errors
+let $error = (globalThis.console && globalThis.console.log) || globalThis.print;
+
+// conversion to string
+let $toString = (value) => {
+  if (value === 0 && 1 / value === -Infinity) return "«-0»";
+  if (value instanceof Error) return "a " + value.constructor.name;
+  if (value === AsyncFunction.prototype) return "the async function prototype"
+  if (value === AsyncGeneratorFunction.prototype) return "the async generator function prototype"
+  if (value === GeneratorFunction.prototype) return "the generator function prototype"
+  if (value === AsyncArrowFunction.prototype) return "the async arrow function prototype"
+  if (value === Object.prototype) return "the object prototype"
+  if (typeof value === "string") return '"' + value + '"';
+  if (typeof value === "function") return "[object Function]";
+  if (typeof value === "object") return "[object Object]";
+  return String(value);
+}
+
+// wrapper of Object.getPrototpyeOf
+let $Object_getPrototypeOf = (o) => {
+  if (o === null || o === undefined) {
+    return undefined;
+  }
+  return Object.getPrototypeOf(o);
+}
+
+// wrapper of Reflect.ownKeys
+let $Reflect_ownKeys = (o) => {
+  if (o === null || o === undefined) {
+    return undefined;
+  }
+  return Reflect.ownKeys(o);
+}
+
+let $isSameValue = (x, y) => {
+  if (x === y) return x !== 0 || 1 / x === 1 / y;
+  return x !== x && y !== y;
+}
+
+// assertion
+let $assert = (mustBeTrue) => {
+  if (mustBeTrue === true) return;
+  $error("Expected true but got " + $toString(mustBeTrue));
+}
+
+// assertion for same values
+$assert.sameValue = function (actual, expected) {
+  if ($isSameValue(actual, expected)) return;
+  $error(
+    "Expected " + $toString(expected) + " but got " + $toString(actual) + "."
+  );
+};
+
+// assertion for same values
+$assert.notSameValue = function (actual, unexpected) {
+  if (!$isSameValue(actual, unexpected)) return;
+  $error(
+    "Not expected " +
+      $toString(unexpected) +
+      " but got " +
+      $toString(actual) +
+      "."
+  );
+};
+
+// assertion for [[Call]]
+$assert.isCallable = function (f) {
+  return typeof f === "function";
+};
+$assert.callable = function (f) {
+  if (!$assert.isCallable(f))
+    $error("Expected " + $toString(f) + " has [[Call]] but does not.");
+};
+$assert.notCallable = function (f) {
+  if ($assert.isCallable(f))
+    $error("Expected " + $toString(f) + " does not have [[Call]] but does.");
+};
+
+// assertion for [[Construct]]
+$assert.isConstructable = function (f) {
+  try {
+    Reflect.construct(function () {}, [], f);
+    return true;
+  } catch (e) {
+    return false;
+  }
+};
+$assert.constructable = function (f) {
+  if (!$assert.isConstructable(f))
+    $error("Expected " + $toString(f) + " has [[Construct]] but does not.");
+};
+$assert.notConstructable = function (f) {
+  if ($assert.isConstructable(f))
+    $error(
+      "Expected " + $toString(f) + " does not have [[Construct]] but does."
+    );
+};
+
+// assertion to compare arrays
+let $compareArray = (a, b) => {
+  if (b.length !== a.length) return false;
+  for (var i = 0; i < a.length; i++) {
+    if (!$isSameValue(a[i], b[i])) return false;
+  }
+  return true;
+}
+
+$assert.compareArray = function (actual, expected, obj) {
+  function format(array) {
+    return "[" + array.map($toString).join(", ") + "]";
+  }
+  function getObjDesc(obj) {
+    var ty = Object.prototype.toString.call(obj);
+    return ty.substring("[object ".length, ty.length - "]".length);
+  }
+
+  if (!Array.isArray(actual)) {
+    $error(
+      "$assert.compareArray requires an array as the first argument but " +
+        $toString(actual) +
+        " given."
+    );
+    return;
+  }
+  if (!Array.isArray(expected)) {
+    $error(
+      "$assert.compareArray requires an array as the second argument but " +
+        $toString(expected) +
+        " given."
+    );
+    return;
+  }
+
+  if ($compareArray(actual, expected)) return;
+  $error(
+    "Expected " +
+      format(expected) +
+      " but got " +
+      format(actual) +
+      " for " +
+      getObjDesc(obj) +
+      "."
+  );
+};
+
+// assertion to compare iterators
+$assert.compareIterator = function (iter, validators) {
+  var i, result;
+  for (i = 0; i < validators.length; i++) {
+    result = iter.next();
+    $error(
+      !result.done,
+      "Expected " +
+        i +
+        " values(s). Instead iterator only produced " +
+        (i - 1) +
+        " value(s)."
+    );
+    validators[i](result.value);
+  }
+  result = iter.next();
+  $error(
+    result.done,
+    "Expected only " + i + " values(s). Instead iterator produced more."
+  );
+  $assert.sameValue(
+    result.value,
+    undefined,
+    "Expected value of `undefined` when iterator completes."
+  );
+};
+
+// verify properties
+let $verifyProperty = (obj, prop, desc) => {
+  // check object
+  if (obj === undefined || obj === null) {
+    $error(
+      "$verifyProperty requires an object but " +
+        $toString(obj) +
+        " given."
+    );
+    return;
+  }
+
+  // check property type
+  var propType = typeof prop;
+  if (propType !== "string" && propType !== "symbol") {
+    $error(
+      "$verifyProperty requires a string or symbol property but " +
+        $toString(prop) +
+        " given."
+    );
+    return;
+  }
+
+  var originalDesc = Object.getOwnPropertyDescriptor(obj, prop);
+
+  // Allows checking for undefined descriptor if it's explicitly given.
+  if (desc === undefined) {
+    $assert.sameValue(originalDesc, undefined);
+    return;
+  }
+
+  var hasOwnProperty = Object.prototype.hasOwnProperty;
+  if (!hasOwnProperty.call(obj, prop)) {
+    $error($toString(obj) + " does not have own property named " + $toString(prop));
+    return;
+  }
+  $assert.notSameValue(desc, null);
+  $assert.sameValue(typeof desc, "object");
+
+  function check(name) {
+    try {
+      if (!hasOwnProperty.call(desc, name)) return;
+      if ($isSameValue(desc[name], originalDesc[name])) return;
+      var message;
+      if (name === "value")
+        message =
+          "descriptor value of " + $toString(prop) + " should be " +
+          $toString(desc.value) +
+          " but " +
+          $toString(originalDesc.value);
+      else
+        message =
+          "descriptor should " + (desc[name] ? "" : "not ") + "be " + name;
+      $error(message);
+    } catch (e) {}
+  }
+  check("value");
+  check("writable");
+  check("enumerable");
+  check("configurable");
+}
+
+// delay checking assertions in JS runtime environment
+// (supported: Node, QuickJs)
+// or wait for 10 microtasks in JS Engine
+// (might be broken if there are more than 10 awaits in program, etc.)
+let $delay = (f) => {
+  var setTimeout = globalThis.setTimeout;
+  import("os")
+    .then((os) => {
+      // qjs
+      setTimeout ??= os?.setTimeout;
+    })
+    .catch(() => {})
+    .then(() => {
+      let p;
+      if(setTimeout)
+        p = new Promise(resolve => setTimeout(resolve, 0))
+      else {
+        p = Promise.resolve();
+        // delay 10 times
+        for(var i = 0; i < 10; i++)
+          p = p.then(() => {});
+      }
+      p.then(f).catch(()=>$error("An exception occured while checking assertions"));
+    });
+}
+
+$assert.sameValue($Object_getPrototypeOf(globalThis["x"]), Function.prototype);
+$assert.sameValue(Object.isExtensible(globalThis["x"]), true);
+$assert.callable(globalThis["x"]);
+$assert.constructable(globalThis["x"]);
+$assert.compareArray($Reflect_ownKeys(globalThis["x"]), ['length', 'name', 'prototype'], globalThis["x"]);
+$assert.sameValue($Object_getPrototypeOf(globalThis["x"]?.["prototype"]), Object.prototype);
+$assert.sameValue(Object.isExtensible(globalThis["x"]?.["prototype"]), true);
+$assert.notCallable(globalThis["x"]?.["prototype"]);
+$assert.notConstructable(globalThis["x"]?.["prototype"]);
+$assert.compareArray($Reflect_ownKeys(globalThis["x"]?.["prototype"]), ['constructor'], globalThis["x"]?.["prototype"]);
+$assert.sameValue(globalThis["x"]?.["prototype"]?.["constructor"], globalThis["x"]);
+$verifyProperty(globalThis["x"]?.["prototype"], "constructor", {
+  writable: true,
+  enumerable: false,
+  configurable: true,
+});
+$verifyProperty(globalThis["x"], "prototype", {
+  writable: false,
+  enumerable: false,
+  configurable: false,
+});
+$verifyProperty(globalThis["x"], "name", {
+  value: "x",
+  writable: false,
+  enumerable: false,
+  configurable: true,
+});
+$verifyProperty(globalThis["x"], "length", {
+  value: 0.0,
+  writable: false,
+  enumerable: false,
+  configurable: true,
+});
+})();
--- src/main/scala/esmeta/js/Target.scala
+++ src/main/scala/esmeta/js/Target.scala
@@ -1,7 +1,10 @@
 package esmeta.js
 
+import esmeta.LINE_SEP
 import esmeta.util.BaseUtils.*
-import esmeta.es.util.injector.ConformTest
+import esmeta.es.Script
+import esmeta.error.{TimeoutException, NoCommandError}
+import esmeta.es.util.injector.*
 import esmeta.es.util.Coverage.Interp
 
 case class Target(
@@ -18,6 +21,46 @@ case class Target(
 
   override def toString = name
 
-  def doConformTest(test: ConformTest): Boolean = ???
+  private val errorPattern = "[\\w]*Error(?=: )".r
+
+  def doConformTest(test: ConformTest): Boolean = {
+    val exitTag = test.exitTag
+    val isNormal = exitTag == NormalTag
+
+    val (concreteExitTag, stdout) =
+      if (!isTrans) {
+        JSEngine
+          .runUsingBinary(_cmd, test.script)
+          .map((NormalTag, _))
+          .recover(engineErrorResolver _)
+          .get
+      } else {
+        JSEngine
+          .run(test.script)
+          .map((NormalTag, _))
+          .recover(engineErrorResolver _)
+          .get
+      }
+    val sameExitTag = exitTag.equivalent(concreteExitTag)
+    val pass = sameExitTag && stdout.isEmpty
+
+    pass
+  }
+
+  private def engineErrorResolver(engineError: Throwable): (ExitTag, String) =
+    engineError match {
+      case e: TimeoutException     => (TimeoutTag, "")
+      case e @ NoCommandError(cmd) => throw e
+      case e =>
+        val msg = e.getMessage
+        val tag = errorPattern.findFirstIn(msg) match {
+          case Some(name) =>
+            ThrowErrorTag(name, msg.split(LINE_SEP).toList.headOption)
+          case _ if msg.contains("TRANSPILE_FAILURE") => TranspileFailTag
+          case _ => ThrowValueTag(esmeta.state.Str(msg))
+        }
+        (tag, "")
+    }
+
   def minimize(code: String): (String, Interp) = ???
 }

8d978b8328ea79a1d2b51ad73da8c0527167fc0f
Kanguk Lee <p51lee@kaist.ac.kr>
2023-03-06 15:54:20 +0900

    Add SKCoverage for potential refactoring

---
+++ src/main/scala/esmeta/es/util/SKCoverage.scala
@@ -0,0 +1,21 @@
+package esmeta.es.util
+
+class SKCoverage(
+  timeLimit: Option[Int] = None,
+  kFs: Int = 0,
+  cp: Boolean = false,
+  isPreFuzz: Boolean = false,
+  nodeViewKMap: Map[String, Int] = Map[String, Int]().withDefaultValue(0),
+  condViewKMap: Map[String, Int] = Map[String, Int]().withDefaultValue(0),
+  pValueMapOpt: Option[Map[String, Double]] = None,
+) extends Coverage(
+    timeLimit,
+    kFs,
+    cp,
+    isPreFuzz,
+    nodeViewKMap,
+    condViewKMap,
+    pValueMapOpt,
+  ) {
+  // someday refactoring
+}

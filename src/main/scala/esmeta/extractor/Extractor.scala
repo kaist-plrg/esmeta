3e790572822fd401afdce8657c0dced7aa037162
Wonho Shin <seyoon1705@gmail.com>
2026-02-11 13:15:47 +0900

    Enable spec patch

--- src/main/scala/esmeta/extractor/Extractor.scala
+++ src/main/scala/esmeta/extractor/Extractor.scala
@@ -109,8 +109,8 @@ class Extractor(
 
     // XXX load Wasm JS Interface algorithms
     // TODO refactor patch routine
-    // executeCmd(s"git reset --hard", s"$WASM_DIR")
-    // executeCmd(s"patch -p1 < $MANUALS_DIR/bugfix/js-api-refinement.patch", s"$WASM_DIR")
+    executeCmd(s"git reset --hard", s"$WASM_DIR")
+    executeCmd(s"patch -p1 < $MANUALS_DIR/bugfix/js-api-refinement.patch", s"$WASM_DIR")
     val wjiDocument = readFile(WJI_SPEC_BS).toHtml
     var wjiJobs = for {
       elem <- wjiDocument.getElems("div[algorithm]")

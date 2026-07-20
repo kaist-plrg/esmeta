bec25bfeeb4a6b4820c445160696d600ad2526fe
Wonho <new170527@kaist.ac.kr>
2026-07-20 15:44:44 +0900

    Revert "Add dependency for InsertFallthroughReturnPass"
    This reverts commit 3817131b83b6a74cb9e36569f8d23cabfb891060.

--- src/main/scala/esmeta/wji/compiler/lowering/InsertFallthroughReturnPass.scala
+++ src/main/scala/esmeta/wji/compiler/lowering/InsertFallthroughReturnPass.scala
@@ -32,10 +32,6 @@ import esmeta.wji.lang.{Algorithm, Instr}
   * as a general safety net besides).
   */
 object InsertFallthroughReturnPass extends LoweringPass:
-  override def requires: Set[LoweringPass] = Set(
-    ExpandPerformReturnResultPass,
-  )
-
   def run(algos: List[Algorithm]): List[Algorithm] =
     algos.map { a =>
       if a.body.exists(_.isInstanceOf[Instr.Return]) then a
